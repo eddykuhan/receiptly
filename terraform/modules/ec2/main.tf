b83c61504011a14b38991d2880a8348b7b287437
Kuhan Dhanyan <eddykuhan92@gmail.com>
2025-12-01 15:27:21 +0800

    fix: correct nginx variable escaping in terraform user_data

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -344,7 +344,7 @@ locals {
         
         # Redirect all other HTTP traffic to HTTPS
         location / {
-            return 301 https://$$server_name$$request_uri;
+            return 301 https://$${server_name}$${request_uri};
         }
     }
     
@@ -379,12 +379,12 @@ locals {
         location / {
             proxy_pass http://localhost:5000;
             proxy_http_version 1.1;
-            proxy_set_header Host $$host;
-            proxy_set_header X-Real-IP $$remote_addr;
-            proxy_set_header X-Forwarded-For $$proxy_add_x_forwarded_for;
+            proxy_set_header Host $${host};
+            proxy_set_header X-Real-IP $${remote_addr};
+            proxy_set_header X-Forwarded-For $${proxy_add_x_forwarded_for};
             proxy_set_header X-Forwarded-Proto https;
-            proxy_set_header X-Forwarded-Host $$host;
-            proxy_cache_bypass $$http_upgrade;
+            proxy_set_header X-Forwarded-Host $${host};
+            proxy_cache_bypass $${http_upgrade};
             proxy_read_timeout 90;
             client_max_body_size 15M;
         }
@@ -405,11 +405,11 @@ locals {
         location / {
             proxy_pass http://localhost:8000;
             proxy_http_version 1.1;
-            proxy_set_header Host $$host;
-            proxy_set_header X-Real-IP $$remote_addr;
-            proxy_set_header X-Forwarded-For $$proxy_add_x_forwarded_for;
+            proxy_set_header Host $${host};
+            proxy_set_header X-Real-IP $${remote_addr};
+            proxy_set_header X-Forwarded-For $${proxy_add_x_forwarded_for};
             proxy_set_header X-Forwarded-Proto https;
-            proxy_cache_bypass $$http_upgrade;
+            proxy_cache_bypass $${http_upgrade};
             proxy_read_timeout 300;
             client_max_body_size 15M;
         }
@@ -430,11 +430,11 @@ locals {
         location / {
             proxy_pass http://localhost:8500;
             proxy_http_version 1.1;
-            proxy_set_header Host $$host;
-            proxy_set_header X-Real-IP $$remote_addr;
-            proxy_set_header X-Forwarded-For $$proxy_add_x_forwarded_for;
+            proxy_set_header Host $${host};
+            proxy_set_header X-Real-IP $${remote_addr};
+            proxy_set_header X-Forwarded-For $${proxy_add_x_forwarded_for};
             proxy_set_header X-Forwarded-Proto https;
-            proxy_cache_bypass $$http_upgrade;
+            proxy_cache_bypass $${http_upgrade};
             proxy_read_timeout 90;
         }
     }
