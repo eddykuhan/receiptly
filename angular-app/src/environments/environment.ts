a7cf8817c2b0836c6a6b718858c7afd46b47d7ce
Kuhan Dhanyan <eddykuhan92@gmail.com>
2025-12-07 12:22:17 +0800

    feat: revert clerkPublishableKey to test key for development

--- angular-app/src/environments/environment.ts
+++ angular-app/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:5188/api',
-  clerkPublishableKey: 'pk_live_Y2xlcmsuY2hlYXAtc3kuY29tJA',  // Set via environment variable in CI/CD
+  clerkPublishableKey: 'pk_test_YnJpZWYtamF5YmlyZC03Mi5jbGVyay5hY2NvdW50cy5kZXYk',  // Set via environment variable in CI/CD
   enableCloudWatchLogging: false
 };
